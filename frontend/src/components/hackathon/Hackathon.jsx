aa30e16263c7705047ea9d423a43705d611005e9
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-27 01:04:01 +0100

    brand image

--- frontend/src/components/hackathon/Hackathon.jsx
+++ frontend/src/components/hackathon/Hackathon.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./hackathon.css"
+import { hashnode, planetscale, xIcon } from './imports'
 
 const Hackathon = () => {
   return (
-    <div>Hackathon</div>
+    <div className="budgetme-brand section-padding">
+      <div >
+        <img className='budgetme-brand-image' src={hashnode} alt="hashnode" />
+      </div>  
+      <div>
+        <img className='budgetme-xicon' src={xIcon} alt="" />
+      </div>
+      <div className='budgetme-planetscale'>
+        <img className='budgetme-brand-image' src={planetscale} alt="planetscale" />
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/hackathon/imports.js
+++ frontend/src/components/hackathon/imports.js
@@ -1,2 +1,5 @@
 import planetscale from "../../assets/planetscale.svg";
 import hashnode from "../../assets/hashnode.svg";
+import xIcon from "../../assets/xSymbol.png";
+
+export { planetscale, hashnode, xIcon };
