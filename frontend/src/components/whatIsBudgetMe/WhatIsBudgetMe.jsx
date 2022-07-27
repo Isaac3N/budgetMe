e5cf042a81a8ee32c9f31a539192ab260228067a
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-27 03:59:45 +0100

    updates, added emoji

--- frontend/src/components/whatIsBudgetMe/WhatIsBudgetMe.jsx
+++ frontend/src/components/whatIsBudgetMe/WhatIsBudgetMe.jsx
@@ -6,7 +6,7 @@ const WhatIsBudgetMe = () => {
   return (
     <div className= "budgetme-what-is-budgetme section-margin" id="wbudgetme">
       <div className="budgetme-what-is-budgetme-feature">
-        <Feature title="What is BudgetMe" 
+        <Feature title="What is BudgetMe 🤔 " 
                   text="To become financially independent, you must turn part of your income into capital; 
                     turn capital into enterprise; turn enterprise into profit; turn a profit into an investment, 
                     and turn the investment into financial independence. - Jim Rohn"/>
@@ -18,13 +18,13 @@ const WhatIsBudgetMe = () => {
         <p>Check out our well Curated Articles </p>
       </div>
       <div className="budgetme-what-is-budgetme-container">
-        <Feature title="Data Analysis"  text="Budget Me provides in depth Data Analysis that would help you keep Track of your Earnings and Expenditures. So you can set your budget wisely"/>
-        <Feature title="Track of your Goals" text="BudgetMe Helps your track your Yearly Financial goals. 
+        <Feature title="Data Analysis 📈"  text="Budget Me provides in depth Data Analysis that would help you keep Track of your Earnings and Expenditures. So you can set your budget wisely"/>
+        <Feature title="Track of your Goals 🏔" text="BudgetMe Helps your track your Yearly Financial goals. 
                                                     We believe that it is imperative for you to track your goals that is why we ,at BudgetMe, 
                                                     integrated a goal tracker so you can keep track of your goals and attain financial freedom"/>
-        <Feature title="Goal Milestones" text="At BudgetMe we believe that it is important to celebrate goals and take notes of setbacks in order to achieve our financial goals.
+        <Feature title="Goal Milestones 🚀" text="At BudgetMe we believe that it is important to celebrate goals and take notes of setbacks in order to achieve our financial goals.
                                                    So we integrated a goal milestone app into our project so we can keep track of those moments and look back on them when need arises"/>
-        <Feature title="Curated Articles" text="We organize the most relevant and up to date financial articles to keep your financially ready."/>
+        <Feature title="Curated Articles 📝  " text="We organize the most relevant and up to date financial articles to keep your financially ready."/>
       </div>
       <div className='footer-message gradient-text'>
         <p>All of These Features Accessible to you Completely Free of Charge.</p>
