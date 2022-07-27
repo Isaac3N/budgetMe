893d6ba0ec00b3a752c5a56f74bb2193eb4de06d
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-27 03:45:58 +0100

    call to action section

--- frontend/src/components/cta/CTA.jsx
+++ frontend/src/components/cta/CTA.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./cta.css"
 
-const CTA = () => {
+const cta = () => {
   return (
-    <div>CTA</div>
+    <div className='budgetme-cta'>
+      <div className='budgetme-cta-content'>
+        <p>Join the Thousands of People Achieving Financial Freedom</p>
+        <h3>SignUp Today to Start Tracking your Expenses and Budgeting Smartly</h3>
+      </div>
+      <div className="budgetme-cta-btn">
+        <button type="button"> Get started </button>
+      </div>
+    </div>
   )
 }
 
-export default CTA
+export default cta
