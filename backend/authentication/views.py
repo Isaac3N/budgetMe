85fe3978ada0a3b52b053e29a0b35e3f04cdd90d
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-28 20:30:41 +0100

    email verification bug fixed for the authentication api

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -67,7 +67,8 @@ class VerifyEmail(views.APIView):
     def get(self, request):
         token = request.GET.get('token')
         try:
-            payload = jwt.decode(token, settings.SECRET_KEY)
+            payload = jwt.decode(
+                token, settings.SECRET_KEY, algorithms='HS256')
             user = User.objects.get(id=payload['user_id'])
             if not user.is_verified:
                 user.is_verified = True
