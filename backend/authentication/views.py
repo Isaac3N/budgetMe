d9031a761112d79f758baafcf031285a40902e6f
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-29 20:57:51 +0100

    register use

---
+++ backend/authentication/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+
+
+class RegisterView(GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
